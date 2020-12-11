47ff298ed9299fa99a059a21bd04740e5ac4c8c1
roshanjoseph23 <roshanjoseph23@gmail.com>
2020-12-11 18:26:44 +0000

    error handling

--- oldbackup.py
+++ oldbackup.py
@@ -4,6 +4,7 @@ import sys
 import ipaddress
 
 usage = 'oldbackup.py <ip> <user> <port>/(default=22)'
+sqlist = []
 
 if len(sys.argv) == 4:
     sftp_host = sys.argv[1]
@@ -17,18 +18,26 @@ else:
     print(usage)
 
 sftp_keyfile = 'project.pem'
-try:
-    if ipaddress.ip_address(sftp_host) and str(sftp_port).isdigit():
-        sftp = pysftp.Connection(host=sftp_host,port=sftp_port,username=sftp_user,private_key=sftp_keyfile)
-        sftp.chdir('mysql/backup')
-        wplist = sftp.listdir()
+if ipaddress.ip_address(sftp_host) and str(sftp_port).isdigit():
+    sftp = pysftp.Connection(host=sftp_host,port=sftp_port,username=sftp_user,private_key=sftp_keyfile)
+    sftp.chdir('mysql/backup')
+    wplist = sftp.listdir()
+    if wplist:
+        for sql in wplist:
+            if sql.endswith('.sql'):
+                sqlist.append(sql)
+        else:
+            print('No SQL backups found..!!!')
+            exit()
         def time(wp):
             return wp.split('-',1)
-        
-        oldest = sorted(wplist,key=time)[0]
+            
+        oldest = sorted(sqlist,key=time)[0]
         print('oldest backup is : ',posixpath.join(sftp.pwd,oldest))
             
         sftp.remove(oldest)
         print('Removed the oldest backup...', oldest)
-except:
+    else:
+        print('Directory is empty...!!!')
+else:
     print('Connection Error...!!!\n',usage)
